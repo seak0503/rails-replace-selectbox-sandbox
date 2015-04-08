8a690145c8527eda822d715e13146434970cd465
Junichi Ito <jit@sonicgarden.jp>
2015-04-09 05:42:05 +0900

    Create seeds.rb.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :category
   belongs_to :sub_category
   validates :name, presence: true
+  validates :category, presence: true
+  validates :sub_category, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.destroy_all
+SubCategory.destroy_all
+Category.destroy_all
+
+{'本' => %w(和書 洋書 コミック 雑誌), '家電・カメラ・AV機器' => %w(家電 カメラ オーディオ), 'ホーム・キッチン' => %w(キッチン用品 雑貨 カーテン)}.each do |category_name, sub_category_names|
+  category = Category.create(name: category_name)
+  sub_category_names.each do |sub_category_name|
+    category.sub_categories.create(name: sub_category_name)
+  end
+end
+
+[%w(Rails入門 和書), %w(コンパクトスピーカー オーディオ), %w(鉄鍋 キッチン用品)].each do |item_name, sub_category_name|
+  sub_category = SubCategory.find_by_name sub_category_name
+  Item.create(name: item_name, category: sub_category.category, sub_category: sub_category)
+end
