d330bdebcca6a8fb4a5c0418d9fd51f099e34fbe
Junichi Ito <jit@sonicgarden.jp>
2015-04-09 07:46:04 +0900

    Add expectations.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name)
+      params.require(:item).permit(:name, :category_id, :sub_category_id)
     end
 end
--- spec/feature/items_spec.rb
+++ spec/feature/items_spec.rb
@@ -10,14 +10,61 @@ feature 'Items' do
     end
   end
   scenario 'Manage items', js: true do
+    # 新規作成画面を開く
     visit root_path
     click_on 'New Item'
 
-    fill_in 'Name', with: 'Rails入門'
-    expect(page).to have_select('Category', options: ['', '本', '家電・カメラ・AV機器'])
-    expect(page).to have_select('Sub category', options: [''])
+    # セレクトボックスのデフォルト値を検証する
+    expect(page).to have_select 'Category', options: ['', '本', '家電・カメラ・AV機器']
+    expect(page).to have_select 'Sub category', options: ['']
+
+    # カテゴリを変更するとサブカテゴリの項目が変わることを検証する
     select '本', from: 'Category'
-    expect(page).to have_select('Sub category', options: ['', '和書', '洋書'])
-    select '和書', from: 'Sub category'
+    expect(page).to have_select 'Sub category', options: ['', '和書', '洋書']
+    select '家電・カメラ・AV機器', from: 'Category'
+    expect(page).to have_select 'Sub category', options: ['', '家電', 'カメラ']
+
+    # バリデーションエラーが発生してもセレクトボックスの内容が保持されることを検証する
+    select '家電', from: 'Sub category'
+    click_on 'Save'
+    expect(page).to have_content "can't be blank"
+    expect(page).to have_select 'Category', selected: '家電・カメラ・AV機器', options: ['', '本', '家電・カメラ・AV機器']
+    expect(page).to have_select 'Sub category', selected: '家電', options: ['', '家電', 'カメラ']
+
+    # 入力値が正しく保存されることを検証する
+    fill_in 'Name', with: '全自動洗濯機'
+    click_on 'Save'
+    expect(page).to have_content 'Item was successfully created.'
+    expect(page).to have_content '全自動洗濯機'
+    expect(page).to have_content '家電・カメラ・AV機器'
+    expect(page).to have_content '家電'
+
+    # 編集画面を開いてもセレクトボックスの内容が保持されることを検証する
+    click_on 'Edit'
+    expect(page).to have_field 'Name', with: '全自動洗濯機'
+    expect(page).to have_select 'Category', selected: '家電・カメラ・AV機器', options: ['', '本', '家電・カメラ・AV機器']
+    expect(page).to have_select 'Sub category', selected: '家電', options: ['', '家電', 'カメラ']
+
+    # データが正しく更新されることを検証する
+    fill_in 'Name', with: 'Everyday Rails Testing with RSpec'
+    select '本', from: 'Category'
+    select '洋書', from: 'Sub category'
+    click_on 'Save'
+    expect(page).to have_content 'Item was successfully updated.'
+    expect(page).to have_content 'Everyday Rails Testing with RSpec'
+    expect(page).to have_content '本'
+    expect(page).to have_content '洋書'
+
+    # 一覧画面での表示内容を検証する
+    click_on 'Back'
+    expect(page).to have_content 'Listing items'
+    expect(page).to have_content 'Everyday Rails Testing with RSpec'
+    expect(page).to have_content '本'
+    expect(page).to have_content '洋書'
+
+    # データの削除が行えることを検証する
+    click_on 'Destroy'
+    expect(page).to have_content 'Item was successfully destroyed.'
+    expect(page).to_not have_content 'Everyday Rails Testing with RSpec'
   end
 end
