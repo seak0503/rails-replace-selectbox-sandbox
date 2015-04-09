72851835a3ae9be14af21b8d34b3736465a2229f
Junichi Ito <jit@sonicgarden.jp>
2015-04-09 09:30:31 +0900

    Add validation to id too.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :category
   belongs_to :sub_category
   validates :name, presence: true
-  validates :category, presence: true
-  validates :sub_category, presence: true
+  validates :category, :category_id, presence: true
+  validates :sub_category, :sub_category_id, presence: true
 end
