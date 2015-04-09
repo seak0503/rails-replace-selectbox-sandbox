d19f0fca37fa95f5120a588bbe3bef0d6aad11dc
Junichi Ito <jit@sonicgarden.jp>
2015-04-09 10:09:27 +0900

    Refactoring.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,6 @@
 class CategoriesController < ApplicationController
   def sub_categories
     category = Category.find(params[:category_id])
-    @sub_categories = category.sub_categories
-    respond_to do |format|
-      format.json
-    end
+    render json: category.sub_categories.select(:id, :name)
   end
 end
--- app/views/categories/sub_categories.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@sub_categories) do |sub_category|
-  # セキュリティ面に考慮して不要な項目を返却しないようにする
-  json.extract! sub_category, :id, :name
-end
