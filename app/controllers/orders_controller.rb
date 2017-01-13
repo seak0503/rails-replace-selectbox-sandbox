a24fa0c29e77814f88eaecbe8c37b40d50821fc4
shyamahira <shyamahira@bbtower.co.jp>
2017-01-14 03:53:11 +0900

    各処理追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,55 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+  end
 
   def new
     @order = Order.new
   end
 
+  def edit
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+    end
+  end
+
+  private
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:contract_number, :order_category_id, :destgroup_ids => [])
+  end
 end
--- app/views/orders/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@orders) do |order|
-  json.extract! order, :id, :contract_number, :customer_number, :delivery_date, :status, :web_url, :order_comment
-  json.url order_url(order, format: :json)
-end
