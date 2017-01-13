d466a65bb3e4cba30f9312e947d31ff2ac7a283f
shyamahira <shyamahira@bbtower.co.jp>
2017-01-13 21:27:12 +0900

    収容依頼作成ベース作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :categories, only: [] do
     resources :sub_categories, only: :index
   end
+  resources :orders do
+    resources :order_categories, only: :index
+  end
   root 'items#index'
 end
